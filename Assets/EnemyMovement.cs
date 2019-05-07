b0555b76e203b632f4f9f32d9364811ed0fe1f7e
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2019-05-07 11:32:06 -0600

    Add

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform[] target;
+    public float rap;
+    private int curr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position != target[curr].position)
+        {
+            Vector3 tar = Vector3.MoveTowards(transform.position, target[curr].position, rap * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(tar);
+        }
+        else
+        {
+            curr = (curr + 1) % target.Length;
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public AudioSource sonido;
+    public Rigidbody cubo;
+    public bool muerto = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sonido = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            cubo.AddForce(0, 0, 8);
+        }
+        if (Input.GetKey("s"))
+        {
+            cubo.AddForce(0, 0, -8);
+        }
+        if (Input.GetKey("a"))
+        {
+            cubo.AddForce(-8, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            cubo.AddForce(8, 0, 0);
+        }
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+            if (cubo)
+            {
+                Destroy(cubo);
+            }
+
+    }
+
+
+}
